cc01afe40c7602dcb7a76a7bcf260efa13916488
BusterWood <busterwood@users.noreply.github.com>
2017-06-12 17:23:12 +0100

    Add reflection-based conversion to and from a sequence of key value pairs

--- IntegrationTest/LocalDbIntegrationTests.cs
+++ IntegrationTest/LocalDbIntegrationTests.cs
@@ -164,6 +164,20 @@ VALUES (3, @when)";
                 Assert.AreEqual(0, result.Count);
             }
         }
+
+        [Test]
+        public void can_pass_table_type()
+        {
+            const string sql = @"select c.* from dbo.Currency c join @ids i on i.id = c.id";
+            using (var cnn = new SqlConnection(mapperTest))
+            {
+                var ids = new int[] { 1 }.ToTableType(new Microsoft.SqlServer.Server.SqlMetaData[] { new Microsoft.SqlServer.Server.SqlMetaData("ID", System.Data.SqlDbType.Int) }, "dbo.IntType");
+                var result = cnn.Query(sql, new { ids }).ToList<Currency>();
+                Assert.IsNotNull(result);
+                Assert.AreEqual(1, result.Count);
+            }
+        }
+
         [Test]
         public void can_read_mutliple_results()
         {
